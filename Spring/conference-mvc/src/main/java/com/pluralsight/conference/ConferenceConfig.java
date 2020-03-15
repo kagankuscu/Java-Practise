7897f5ae161f5f1056970734d3819df5391d60a1
Kagank <kagan-kuscu@hotmail.com.tr>
2020-03-15 20:47:18 +0200

    Using thymeleaf in Spring MVC Views

--- Spring/conference-mvc/src/main/java/com/pluralsight/conference/ConferenceConfig.java
+++ Spring/conference-mvc/src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -2,6 +2,8 @@ package com.pluralsight.conference;
 
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
@@ -10,12 +12,18 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
         resourceHandlerRegistry.addResourceHandler("/files/**")
@@ -41,12 +49,39 @@ public class ConferenceConfig implements WebMvcConfigurer {
         return lci;
     }
 
+    // @Bean
+    // public ViewResolver viewResolver() {
+    //     InternalResourceViewResolver bean = new InternalResourceViewResolver();
+    //     bean.setPrefix("/WEB-INF/jsp/");
+    //     bean.setSuffix(".jsp");
+    //     bean.setOrder(0);
+    //     return bean;
+    // }
+    
+
     @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setPrefix("/WEB-INF/jsp/");
-        bean.setSuffix(".jsp");
-        bean.setOrder(0);
-        return bean;
-    }    
+    public ViewResolver thymeleafResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(0);
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/view/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
 }
