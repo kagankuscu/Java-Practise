24f0e3e76862ba8c269ad0eef36d9174e5a8eff4
Kagank <kagan-kuscu@hotmail.com.tr>
2020-03-17 00:15:54 +0200

    Added asynchrous method

--- Java Console App/HttpClient/src/app/LinkValidatorAsyn.java
+++ Java Console App/HttpClient/src/app/LinkValidatorAsyn.java
@@ -1,33 +1,33 @@
 package app;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
-public class LinkValidator {
+public class LinkValidatorAsyn {
     private static HttpClient client;
+
     public static void main(String[] args) throws Exception {
         client = HttpClient.newHttpClient();
-        
-        Files.lines(Path.of("urls.txt")).map(LinkValidator::validateLink).forEach(System.out::println);
+
+        var futures = Files.lines(Path.of("urls.txt")).map(LinkValidatorAsyn::validateLink)
+                .collect(Collectors.toList());
+
+        futures.stream().map(CompletableFuture::join).forEach(System.out::println);
     }
 
-    private static String validateLink(String link) {
+    private static CompletableFuture<String> validateLink(String link) {
         HttpRequest request = HttpRequest.newBuilder(URI.create(link))
         .GET()
         .build();
 
-        try {
-            HttpResponse<Void> response = client.send(request, HttpResponse.BodyHandlers.discarding());
-            return reponseToString(response);
-        } catch (IOException | InterruptedException e) {
-            e.getStackTrace();
-            return String.format("%s -> %s", link, false);
-        }
+        return client.sendAsync(request, HttpResponse.BodyHandlers.discarding())
+                .thenApply(LinkValidatorAsyn::reponseToString).exceptionally(e -> String.format("%s -> %s", link, false));
     }
 
     private static String reponseToString(HttpResponse<Void> response) {
