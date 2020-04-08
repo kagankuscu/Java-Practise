8b945136b4038d81de4a3e6153fdb6df87b8c35f
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-04-08 22:00:53 +0300

    initializer context param, initparam from the web.xml

--- Java EE/kgn/src/main/java/com/kgn/SimpleServlet.java
+++ Java EE/kgn/src/main/java/com/kgn/SimpleServlet.java
@@ -3,15 +3,25 @@ package com.kgn;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = { "/home", "*.do" })
+@WebServlet(urlPatterns = { "/home", "*.do" }, name = "SimpleServlet",  
+            initParams = {@WebInitParam(name = "ProductName",
+                                        value = "Welcome Application")})
 public class SimpleServlet extends HttpServlet {
 
     String appName = "My Application";
+
+    @Override
+    public void init() throws ServletException {
+        // appName = getInitParameter("ProductName");
+        appName = getServletContext().getInitParameter("ProductName"); // Context parameter
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // readinng
