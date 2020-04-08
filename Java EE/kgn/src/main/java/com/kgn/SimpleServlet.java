a922a8b6abb832c56ded38346b956fd54ea6a17b
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-04-08 21:40:37 +0300

    Reading an http request.

--- Java EE/kgn/src/main/java/com/kgn/SimpleServlet.java
+++ Java EE/kgn/src/main/java/com/kgn/SimpleServlet.java
@@ -8,11 +8,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns =  {"/home", "*.do"})
+@WebServlet(urlPatterns = { "/home", "*.do" })
 public class SimpleServlet extends HttpServlet {
 
+    String appName = "My Application";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello World");
+        // readinng
+        String name = req.getParameter("name");
+
+        if (name != null) {
+            resp.setContentType("text/xml");
+            resp.getWriter().printf("<application>" + 
+                    "<name> Hello %s</name>" + 
+                    "<product>%s</product>" +
+                    "</application>" ,name, appName);
+        } else {
+            resp.sendRedirect("index.jsp");
+        }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)  throws ServletException, IOException {
+        String name = req.getParameter("name");
+
+        if ( name != null && name != "") {
+            resp.getWriter().printf("Hello %s", name);
+        } else {
+            resp.sendRedirect("index.jsp");
+        }
+    }
+
 }
